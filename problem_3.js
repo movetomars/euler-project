f4b3728a68f706b655e3e766d9308aebd0c000ab
movetomars <maia.petee@gmail.com>
2015-03-27 16:40:18 -0600

    Updated Solution 3

--- problem_3.js
+++ problem_3.js
@@ -1,9 +1,25 @@
 var massive = 600851475143;
 var factor = 0;
 
-for(i = 2; i < 1000; i++) {
-    if(massive % i == 0 && isPrime(i)) {
-        factor = i;
+function isPrime(k){
+    if(k < 2 || k != Math.round(k)) {
+        return false;
+    }
+
+    var isPrime = true;
+
+    for(var i = 2; i <= Math.sqrt(k); i++) {
+        if(k % i == 0) {
+            isPrime = false;
+        }
+    }
+
+    return isPrime;
+}
+
+for(var j = 2; j < 1000; j++) {
+    if(massive % j == 0 && isPrime == true) {
+        factor = j;
     }
 
     alert(factor);
